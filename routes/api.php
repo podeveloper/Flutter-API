a910445a8e3651ee4ce1472f52e86f674f22ec27
Yasin Korkmaz <theacherx@gmail.com>
2023-10-09 13:06:09 +0300

    Proper Auth Controller

--- routes/api.php
+++ routes/api.php
@@ -24,20 +24,6 @@ Route::group(['middleware' => 'auth:sanctum'], function (){
     Route::apiResource('transactions', \App\Http\Controllers\Api\TransactionController::class);
 });
 
-Route::post('/sanctum/token', function (Request $request) {
-    $request->validate([
-        'email' => 'required|email',
-        'password' => 'required',
-        'device_name' => 'required',
-    ]);
-
-    $user = \App\Models\User::where('email', $request->email)->first();
-
-    if (! $user || ! \Illuminate\Support\Facades\Hash::check($request->password, $user->password)) {
-        throw \Illuminate\Validation\ValidationException::withMessages([
-            'email' => ['The provided credentials are incorrect.'],
-        ]);
-    }
-
-    return $user->createToken($request->device_name)->plainTextToken;
-});
+Route::post('/auth/login',[\App\Http\Controllers\Api\AuthController::class, 'login']);
+Route::post('/auth/register',[\App\Http\Controllers\Api\AuthController::class, 'register']);
+Route::post('/auth/logout',[\App\Http\Controllers\Api\AuthController::class, 'logout']);
